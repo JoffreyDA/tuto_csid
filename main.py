bf9b5746a06a9f9f2eb60bd8bbae9b3f8d0589c3
unknown <gatl.deamorimjoffrey@gmail.com>
2025-03-17 11:43:09 +0100

    Ajout du workflow GitHub Actions

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # main.py
 def add(a, b):
     return a + b
+
+
+def prout(a):
+    return a 
+
 if __name__ == "__main__":
     print("3 + 5 =", add(3, 5))
--- test_main.py
+++ test_main.py
@@ -1,6 +1,13 @@
 # test_main.py
-from main import add
+from main import add, prout
+
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
+
+def test_prout():
+    assert prout(2) == 4
+    assert prout(3) == 9
+    assert prout(0) == 0
